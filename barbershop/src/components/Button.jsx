686e18cf032a75912b25531e73e4d1e5f81e4d47
xcaio <caiosantos1703@gmail.com>
2025-09-05 14:14:40 -0300

    fix: update WhatsApp link text for clarity in scheduling

--- barbershop/src/components/Button.jsx
+++ barbershop/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <a href="https://wa.me/+5531995925295/?text=Olá, gostaria de marcar um horário" target='_blank' className='mx-5 px-0 py-0 text-sm md:mx-5 px-5 py-5 shadow-2xl bg-button rounded-2xl cursor-pointer hover:scale-110 font-bold text-text'>
+    <a href="https://wa.me/+5531995925295/?text=Olá, gostaria de agendar um horário com você!" target='_blank' className='mx-5 px-0 py-0 text-sm md:mx-5 px-5 py-5 shadow-2xl bg-button rounded-2xl cursor-pointer hover:scale-110 font-bold text-text'>
       <button className='cursor-pointer'>Agende seu Horário</button>
 
     </a>
