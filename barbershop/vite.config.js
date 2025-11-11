37b7c7d3401ae1273486a88465be762d9a25530a
Caio Santos <50676424+xCaio@users.noreply.github.com>
2025-11-11 10:05:23 -0300

    Update Vite config root path to relative

--- barbershop/vite.config.js
+++ barbershop/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  root: 'barbershop',     
+  root: './barbershop',     
   base: '/',              
   build: {
     outDir: './dist',    
