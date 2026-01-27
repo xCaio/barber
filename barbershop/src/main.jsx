b4e2082b39b1ace63e9a6972cef80cfbed5ac4e0
xcaio <caiosantos1703@gmail.com>
2026-01-27 13:06:12 -0300

    Fix import path for Vercel Analytics component in main.jsx

--- barbershop/src/main.jsx
+++ barbershop/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {Analytics} from "@vercel/analytics/next"
+import {Analytics} from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
